213dace764bd77bfbaa09c9bfe87c5410070b72f
Tate Eskew <tate.eskew@gmail.com>
2012-09-15 22:17:56 -0500

    changing some stuff in bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -17,10 +17,12 @@ mkdir -p ~/src
 
 ensure_link "lib/dotfiles/hgrc" ".hgrc"
 
-test -d ~/.hg-git/    || hg clone "bb://durin42/hg-git/" "$HOME/.hg-git"
+test -d ~/.hg-git/    || hg clone "https://bitbucket.org/durin42/hg-git" "$HOME/.hg-git"
 test -d ~/lib/dulwich || git clone "git://github.com/jelmer/dulwich.git" "$HOME/lib/dulwich"
 
-ensure_link "lib/dulwich/dulwich" "lib/hg/hg/dulwich"
+# install dulwich
+sudo python "$HOME/lib/dulwich/setup.py install"
+# ensure_link "lib/dulwich/dulwich" "lib/hg/hg/dulwich"
 
 test -d ~/lib/dotfiles || git clone https://github.com/tateeskew/dotfiles.git ~/lib/dotfiles
 
@@ -30,7 +32,6 @@ ensure_link "lib/dotfiles/gitconfig"           ".gitconfig"
 ensure_link "lib/dotfiles/ackrc"               ".ackrc"
 ensure_link "lib/dotfiles/weechat"             ".weechat"
 ensure_link "lib/dotfiles/hgignore"            ".hgignore"
-ensure_link "lib/dotfiles/grc"                 ".grc"
 ensure_link "lib/dotfiles/pythonrc.py"         ".pythonrc.py"
 
 echo completed
