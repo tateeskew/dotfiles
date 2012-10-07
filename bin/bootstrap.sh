b8b98129f18729238f5624e7e1c8090f4ec90efd
Tate Eskew <tate.eskew@gmail.com>
2012-10-06 20:48:34 -0500

    changed logic in bootstrap to do some better tests. ran into problems when already having files installed

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,21 +2,21 @@
 #still getting this right.  this may blow up your machine. may. blow. up.
 #i have a salt machine setup to bootstrap the prerequisites. you'll have to do that on your own.
 
+# let's set some stuff up
+USER=`id -un`
+PROGRAMS=('python' 'git' 'weechat-curses' 'hg' 'vim' 'zsh' 'autojump')
+FILES=('~/.vim' '~/.vimrc' '~/.gitconfig' '~/.ackrc' '~/.weechat' '~/.hgignore' '~/.pythonrc.py' '~/.zshrc' '~/.hgrc') 
+
 #exit if something fails
 set -e
 
 echo "Prerequisites: python git weechat hg ack zsh vim autojump (if CentOS, autojump-zsh)"
 
-command -v python >/dev/null 2>&1 || { echo >&2 "I require Python, but it's not installed.  Aborting."; exit 1; }
-command -v git >/dev/null 2>&1 || { echo >&2 "I require git, but it's not installed.  Aborting."; exit 1; }
-command -v weechat-curses >/dev/null 2>&1 || { echo >&2 "I require weechat, but it's not installed.  Aborting."; exit 1; }
-command -v hg >/dev/null 2>&1 || { echo >&2 "I require mercurial, but it's not installed.  Aborting."; exit 1; }
-command -v vim >/dev/null 2>&1 || { echo >&2 "I require vim, but it's not installed.  Aborting."; exit 1; }
-command -v zsh >/dev/null 2>&1 || { echo >&2 "I require zsh, but it's not installed.  Aborting."; exit 1; }
-command -v autojump >/dev/null 2>&1 || { echo >&2 "I require autojump, but it's not installed.  Aborting."; exit 1; }
+for program in "${PROGRAMS[@]}"; do
+    command -v $program >/dev/null 2>&1 || { echo >&2 "I require $program, but it's not installed.  Aborting."; exit 1; }
+done
 
-
-ensure_link() {
+symlink() {
     test -L "$HOME/$2" || ln -s "$HOME/$1" "$HOME/$2"
 }
 
@@ -25,39 +25,34 @@ mkdir -p ~/lib/virtualenvs
 mkdir -p ~/bin
 mkdir -p ~/sourcecode
 
-ensure_link "lib/dotfiles/hgrc" ".hgrc"
-
-test -d ~/.hg-git/    || hg clone "https://bitbucket.org/durin42/hg-git" "$HOME/.hg-git"
-
-
 # install oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | /bin/bash
 
 test -d ~/lib/dotfiles || git clone --recursive https://github.com/tateeskew/dotfiles.git ~/lib/dotfiles
 
-# make backups of .bashrc or .zshrc and .vim directory
-if [ -f "~/.bashrc" ]; then
-    mv ~/.bashrc ~/.bashrc.orig
-fi
+# make backups of current dotfiles
+BACKUPDIR=~/$USER-dotfilebackup
+mkdir $BACKUPDIR
 
-if [ -f "~/.zshrc" ]; then
-    mv ~/.zshrc ~/.zshrc.orig
-fi
+for file in "${FILES[@]}"; do
+    if [ -e "$file" ]; then
+        mv $file $BACKUPDIR/$file-backup
+    fi
+done
 
-if [ -d "~/.vim" ]; then
-    mv ~/.vim ~/.vim-backup
-fi
+# make symlinks
+symlink "lib/dotfiles/hgrc"                ".hgrc"
+symlink "lib/dotfiles/vim"                 ".vim"
+symlink "lib/dotfiles/vimrc"               ".vimrc"
+symlink "lib/dotfiles/gitconfig"           ".gitconfig"
+symlink "lib/dotfiles/ackrc"               ".ackrc"
+symlink "lib/dotfiles/weechat"             ".weechat"
+symlink "lib/dotfiles/hgignore"            ".hgignore"
+symlink "lib/dotfiles/pythonrc.py"         ".pythonrc.py"
+symlink "lib/dotfiles/zshrc"               ".zshrc"
+symlink "lib/dotfiles/tates.zsh-theme"     ".oh-my-zsh/themes/tates.zsh-theme"
 
-
-ensure_link "lib/dotfiles/vim"                 ".vim"
-ensure_link "lib/dotfiles/vimrc"               ".vimrc"
-ensure_link "lib/dotfiles/gitconfig"           ".gitconfig"
-ensure_link "lib/dotfiles/ackrc"               ".ackrc"
-ensure_link "lib/dotfiles/weechat"             ".weechat"
-ensure_link "lib/dotfiles/hgignore"            ".hgignore"
-ensure_link "lib/dotfiles/pythonrc.py"         ".pythonrc.py"
-ensure_link "lib/dotfiles/bashrc"              ".bashrc"
-ensure_link "lib/dotfiles/zshrc"               ".zshrc"
-ensure_link "lib/dotfiles/tates.zsh-theme"     ".oh-my-zsh/themes/tates.zsh-theme"
+# install hg-git
+test -d ~/.hg-git/ || hg clone "https://bitbucket.org/durin42/hg-git" "$HOME/.hg-git"
 
 echo "Completed.  If during the Oh-My-Zsh install above you noticed an Authentication failure, you will need to run ' chsh -s $(which zsh) ' again, as it will prompt you for your password."
