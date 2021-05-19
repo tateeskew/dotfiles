cc78ac94888f53078881e07b49640d598af414f0
Tate Eskew <tate.eskew@gmail.com>
2021-05-18 22:44:01 -0500

    add new backup arch packages script

--- bin/backup-arch-packages.sh
+++ bin/backup-arch-packages.sh
@@ -2,5 +2,7 @@
 
 # crontab -e
 # 0 18 * * * /home/teskew/bin/backup-arch-packages.sh
+# To Restore packages from the arch.packages list run the following
+# https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#List_of_installed_packages
 
 pacman -Qe | awk '{ print $1 }' > /home/teskew/lib/dotfiles/arch.packages
